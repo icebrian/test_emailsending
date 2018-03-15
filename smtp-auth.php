f61881dfafd43c7fff459fc52564afb6e81b3193
Istvan Cebrian <istvan.cebrian@gmail.com>
2018-03-15 22:48:32 +0000

    clean-up

--- smtp-auth.php
+++ smtp-auth.php
@@ -11,22 +11,22 @@ require 'PHPMailer/SMTP.php';
 //Load composer's autoloader
 //require 'vendor/autoload.php';
 
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+$mail = new PHPMailer(true);        // Passing `true` enables exceptions
 try {
     //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = '172.16.4.4';  // Specify main and backup SMTP servers
-    $mail->SMTPAuth = false;                               // Enable SMTP authentication
-    $mail->Username = '';                 // SMTP username
-    $mail->Password = '';                           // SMTP password
-    $mail->SMTPSecure = '';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 25;                                    // TCP port to connect to
+    $mail->SMTPDebug = 2;           // Enable verbose debug output
+    $mail->isSMTP();                // Set mailer to use SMTP
+    $mail->Host = '172.16.4.4';     // Specify main and backup SMTP servers
+    $mail->SMTPAuth = false;        // Enable SMTP authentication
+    $mail->Username = '';           // SMTP username
+    $mail->Password = '';           // SMTP password
+    $mail->SMTPSecure = '';         // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 25;               // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('smtpmailer@icebrian.ml', 'Method: SMTP Auth');
-    $mail->addAddress($_POST['to'], 'Your Name');     // Add a recipient
-    //$mail->addAddress('ellen@example.com');               // Name is optional
+    $mail->setFrom('smtpmailer@icebrian.ml', 'SMTP Mailer');
+    $mail->addAddress($_POST['to']);                        // Add a recipient
+    //$mail->addAddress('ellen@example.com', 'Your Name');  // Name is optional
     //$mail->addReplyTo('ellen@example.com', 'Information');
     //$mail->addCC('cc@example.com');
     //$mail->addBCC('bcc@example.com');
@@ -37,7 +37,7 @@ try {
 
     //Content
     $mail->isHTML(true);                                  	// Set email format to HTML
-    $mail->Subject = 'SMTP Auth Email Sending';
+    $mail->Subject = 'Method: SMTP Auth';
     $mail->Body    = $_POST['comments'];					// HTML
     $mail->AltBody = $_POST['comments'];					// Non-HTML
 
